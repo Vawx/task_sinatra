ca29f8eca88088123b29db46e0fb3b08d42845ef
Kyle Langley and Mike Mahoney <mahonmr@me.com>
2016-01-12 09:12:49 -0800

    add rspec for clear tasks

--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -2,6 +2,22 @@ require('rspec')
 require('task')
 
 describe(Task) do
+  before() do
+    Task.clear
+  end
+
+  describe(".all") do
+    it("is empty at first") do
+      expect(Task.all).to(eq([]))
+    end
+  end
+  describe(".clear") do
+    it("empties out all of the saved tasks") do
+      Task.new("Wash the lion").save
+      Task.clear
+      expect(Task.all).to(eq([]))
+    end
+  end
   describe("#description") do
     it("lets give it a description") do
       test_task = Task.new("scrub the zebra")
@@ -13,12 +29,6 @@ describe(Task) do
       test_task = Task.new("wash the lion")
       test_task.save()
       expect(Task.all).to(eq([test_task]))
-    end
-  end
-  describe(".all") do
-    it("is empty at first") do
-      Task.clear
-      expect(Task.all).to(eq([]))
     end
   end
 end
